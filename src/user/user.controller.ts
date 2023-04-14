b51ce4d8c1d9705158c4b21341d5e8f076c5e68d
Marcel "Malib" Beliš <belis.marcel@gmail.com>
2023-04-14 08:53:50 +0200

    users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,16 @@
 import {Controller, Get, Post} from '@nestjs/common';
+import {MustBeEntityError} from "typeorm";
+import {UserService} from "./user.service";
 
 @Controller('users')
 export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ) {
+    }
 
-
+    @Get()
+    findAllUsers(){
+        return this.userService.findAllUsers();
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import {User} from "../entities/user.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {CreateUserDto} from "./dto/create-user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {
+
+    }
+
+    async findAllUsers(): Promise<User[]> {
+
+        return this.userRepository.find();
+    }
+
+    async createUser(createUserDto:CreateUserDto):Promise<User>{
+        const newUser = this.userRepository.create(createUserDto);
+        return this.userRepository.save(newUser);
+    }
+}
