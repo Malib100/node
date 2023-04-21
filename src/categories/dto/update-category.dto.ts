49c57f6b07dcc4c5e1228972549752e525752649
Marcel "Malib" Beliš <belis.marcel@gmail.com>
2023-04-21 08:26:40 +0200

    update-category.dto.ts

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -5,7 +5,7 @@ import {IsNotEmpty, IsOptional} from "class-validator";
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
 
     @IsOptional()
-    title:string;
+    title?:string;
 
     @IsOptional()
     description?:string
